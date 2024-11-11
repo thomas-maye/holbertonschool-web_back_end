eae3966359a1c59c88800e8e1d55ce52616771dc
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-11 22:41:53 +0100

    Try to fix it

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    return initialNumber + expansion1989 + expansion2019;
+  expansion1989 = expansion1989 === undefined ? 89 : expansion1989;
+  expansion2019 = expansion2019 === undefined ? 19 : expansion2019;
+  return initialNumber + expansion1989 + expansion2019;
 }
