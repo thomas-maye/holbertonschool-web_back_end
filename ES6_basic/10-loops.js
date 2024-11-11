e98521a82bdda1c5bf74f72432d85cf51e511a25
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-11 22:03:37 +0100

    Try to fix it

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-    for (let idx in array) {
-        let value = array[idx];
+    for (let [idx, value] of array.entries()) {
         array[idx] = appendString + value;
     }
 
