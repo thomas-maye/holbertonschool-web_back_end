a2c893205c29fa5f01f82cd2cfff90ff0ef7dbcd
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-11 22:43:12 +0100

    Return to the previous version

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  expansion1989 = expansion1989 === undefined ? 89 : expansion1989;
-  expansion2019 = expansion2019 === undefined ? 19 : expansion2019;
-  return initialNumber + expansion1989 + expansion2019;
+    return initialNumber + expansion1989 + expansion2019;
 }
