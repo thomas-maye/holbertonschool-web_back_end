6b9b5743d761e4612090183f324de774c3980bff
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-11 22:52:38 +0100

    Refactor getSumOfHoods to use default parameters for expansion1989 and expansion2019

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  expansion1989 = expansion1989 || 89;
-  expansion2019 = expansion2019 || 19;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
