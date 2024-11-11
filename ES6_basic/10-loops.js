9a24cd10b0e977ccb03063205c840c2bd36aeea0
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-11 22:01:35 +0100

    Task 8, 9 & 10: - 8 : No need to create empty objects before adding in properties - 9 : ES6 method properties - 10 : For...of Loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    for (let idx in array) {
+        let value = array[idx];
+        array[idx] = appendString + value;
+    }
+
+    return array;
+}
