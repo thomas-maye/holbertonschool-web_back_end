bd9bc24231394205bcfcf183736978073c499459
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-12 16:00:01 +0100

    Task 0, 1, & 2 : - 0 : create a class and a constructor of the class - 1 : import a class, and create a function that initialize the number of students in the classrooms - 2 : create a class with constructors attributs, and for each create a getter and a setter

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set name(newName) {
+        if (typeof newName === 'string') {
+            this._name = newName;
+        } else {
+            throw new TypeError('Name must be a string');
+        }
+    }
+
+    set length(newLength) {
+        if (typeof newLength === 'number') {
+            this._length = newLength;
+        } else {
+            throw new TypeError('Length must be a number');
+        }
+    }
+
+    set students(newStudents) {
+        if (Array.isArray(newStudents)) {
+            this._students = newStudents;
+        } else {
+            throw new TypeError('Students must be an array');
+        }
+    }
+}
