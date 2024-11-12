430ef8146a6599015867ed033e6a788c5cfd878e
Thomas Maye <thomas.maye.33@gmail.com>
2024-11-12 16:09:09 +0100

    Try to fix the task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -38,7 +38,7 @@ export default class HolbertonCourse {
         if (Array.isArray(newStudents)) {
             this._students = newStudents;
         } else {
-            throw new TypeError('Students must be an array');
+            throw new TypeError('Students must be an array of Strings');
         }
     }
 }
