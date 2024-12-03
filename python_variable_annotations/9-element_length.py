b841a80079b2833935eb54d79b5cffe76cbdcbff
Thomas Maye <thomas.maye.33@gmail.com>
2024-12-03 15:16:52 +0000

    Add docstring to element_length function for clarity

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,4 +6,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Function to return a list of tuples with the length of each element """
     return [(i, len(i)) for i in lst]
