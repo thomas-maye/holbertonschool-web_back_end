f8fedb1d9053e50234235c2f13c8d4e30c697f8e
Thomas Maye <thomas.maye.33@gmail.com>
2024-12-04 14:04:03 +0000

    Try to fixc my task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-""""An asynchronous coroutine that takes in an integer argument"""
-
+"""An asynchronous coroutine that takes in an integer argument"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that takes in an integer argument"""
     delays = [wait_random(max_delay) for i in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    completed_delays = []
+    for delay in asyncio.as_completed(delays):
+        completed_delays.append(await delay)
+    return completed_delays
