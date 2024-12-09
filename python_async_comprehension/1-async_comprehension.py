5dc65ce1e43c7ee5c9ec57b376846ca1d337a09f
Thomas Maye <thomas.maye.33@gmail.com>
2024-12-09 13:48:33 +0000

    Update async_comprehension to use List for type hinting

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,13 @@
 """Async Comprehensions"""
 
 
+from typing import List
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing
     over async_generator."""
     return [i async for i in async_generator()]
