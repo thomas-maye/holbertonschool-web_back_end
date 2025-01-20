0904fae10d47e4722155a3f9e97fabbc7c4c745b
Thomas Maye <thomas.maye.33@gmail.com>
2025-01-20 13:33:31 +0100

    Task 2&3 : Add countStudents function to read CSV file and print student counts per field

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+// Export a function named countStudents that reads a CSV file and prints the number of students per field.
+const fs = require('fs');
+
+const countStudents = (path) => {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const lines = data.split('\n').filter((line) => line);
+        const fields = {};
+        lines.forEach((line) => {
+        const student = line.split(',');
+        if (!fields[student[3]]) fields[student[3]] = [];
+        fields[student[3]].push(student[0]);
+        });
+        console.log(`Number of students: ${lines.length - 1}`);
+        for (const field in fields) {
+        if (field) {
+            const list = fields[field].join(', ');
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+        }
+        }
+    } catch (error) {
+        throw new Error('Cannot load the database');
+    }
+    };
+
+module.exports = countStudents;
---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+// Create a function named countStudents that reads a CSV file and prints the number of students per field.
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.length > 0);
+      console.log(`Number of students: ${lines.length - 1}`);
+      const fields = {};
+      for (let i = 1; i < lines.length; i++) {
+        const student = lines[i].split(',');
+        if (fields[student[3]]) {
+          fields[student[3]].push(student[0]);
+        } else {
+          fields[student[3]] = [student[0]];
+        }
+      }
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        }
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
