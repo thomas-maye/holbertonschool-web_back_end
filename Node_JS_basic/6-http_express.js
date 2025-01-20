95a5d914bded72f6328b5730cee8f6b51b627d70
Thomas Maye <thomas.maye.33@gmail.com>
2025-01-20 15:41:12 +0100

    Task 6 : Add a simple HTTP server using Express to respond with a greeting

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+// Create a simple HTTP server using Express
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
